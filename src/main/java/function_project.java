4a269aed93454d18ff2bfe6899580519c25ec329
Ankitz007 <ankitkumarsingh11061999@gmail.com>
2020-07-16 02:39:45 +0530

    minor changes

--- src/main/java/function_project.java
+++ src/main/java/function_project.java
@@ -7,18 +7,18 @@ import static com.mongodb.client.model.Projections.fields;
 
 public class function_project implements aggregate_stage{
 
-    ArrayList<String> project_fields = new ArrayList<String>() ;
+    ArrayList<String> project_fields;
     function_project(ArrayList<String> pfs){
         project_fields = pfs;
     }
-
     ArrayList<String> m = new ArrayList<String>(project_fields);
 
-//    for(int i=0;i<m.size();i++){
-//        m.set(i, "\""+m.get(i)+"\"");
-//    }
-
-
+    public void field_formation(){
+        for(int i=0;i<m.size();i++){
+            m.set(i, "\""+m.get(i)+"\"");
+        }
+    }
+    String a2s = String.join(", ", m);
     @Override
     public Bson generated_query() {
         Bson project = project(fields());
